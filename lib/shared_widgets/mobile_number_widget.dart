e2f1768c461609e1e47816c788dddea37a4c8aec
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-04-10 23:54:18 +0300

    working on login mobile number

--- lib/shared_widgets/mobile_number_widget.dart
+++ lib/shared_widgets/mobile_number_widget.dart
@@ -5,54 +5,72 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:country_pickers/country.dart';
 import 'package:flutter/services.dart';
 
-class MobileNumberField extends StatelessWidget {
+class MobileNumberField extends StatefulWidget {
   final TextEditingController controller;
   final Function(String) selectedCountry;
   const MobileNumberField({required this.controller, required this.selectedCountry, super.key});
 
   @override
+  State<MobileNumberField> createState() => _MobileNumberFieldState();
+}
+
+class _MobileNumberFieldState extends State<MobileNumberField> {
+  @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.ltr,
-      child: Container(
-        height: 50,
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          border: Border.all(color: Colors.grey),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CountryPickerDropdown(
-              iconSize: 16,
-              initialValue: 'JO',
-              itemFilter: (c) => countryCodeFiltered.contains(c.isoCode),
-              itemBuilder: _buildDropdownItem,
-              sortComparator: (Country a, Country b) => a.isoCode.compareTo(b.isoCode),
-              onValuePicked: (Country country) {
-                selectedCountry(country.phoneCode);
-              },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Container(
+              height: 60,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.grey),
+              ),
+              child: CountryPickerDropdown(
+                iconSize: 16,
+                initialValue: 'JO',
+                itemFilter: (c) => countryCodeFiltered.contains(c.isoCode),
+                itemBuilder: _buildDropdownItem,
+                sortComparator: (Country a, Country b) => a.isoCode.compareTo(b.isoCode),
+                onValuePicked: (Country country) {
+                  widget.selectedCountry(country.phoneCode);
+                },
+              ),
             ),
-            VerticalDivider(color: Colors.grey[900]),
-            Expanded(
-              child: TextField(
-                maxLength: 17,
-                keyboardType: TextInputType.number,
-                controller: controller,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.only(top: 20),
-                  border: InputBorder.none,
-                  counterText: "",
-                  hintStyle: const TextStyle(color: Colors.black54, fontSize: 20),
-                  hintText: AppLocalizations.of(context)!.entermobilenumber,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Container(
+                height: 60,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: TextField(
+                  maxLength: 17,
+                  keyboardType: TextInputType.number,
+                  controller: widget.controller,
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(top: 0, bottom: 4),
+                    border: InputBorder.none,
+                    counterText: "",
+                    hintStyle: const TextStyle(color: Colors.black54, fontSize: 20),
+                    hintText: AppLocalizations.of(context)!.entermobilenumber,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
